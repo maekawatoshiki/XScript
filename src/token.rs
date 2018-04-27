d3d280253d16fa7b16aac4343f9a286633104e40
maekawatoshiki <konndennsa@gmail.com>
2018-04-27 22:24:36 +0900

    add token definitions

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,9 @@
+pub struct Token {
+    kind: TokenKind, // The kind of token
+    pos: usize,      // The position in code this token appears first
+}
+
+#[derive(PartialEq, Debug, Clone)]
 enum TokenKind {
     Identifier(String),
     Int(i64),
@@ -6,7 +12,52 @@ enum TokenKind {
     Symbol(Symbol),
 }
 
-pub struct Token {
-    kind: TokenKind, // The kind of token
-    pos: usize,      // The position in code this token appears first
+#[derive(PartialEq, Debug, Clone)]
+pub enum Symbol {
+    OpeningParen,
+    ClosingParen,
+    OpeningBrace,
+    ClosingBrace,
+    OpeningBoxBracket,
+    ClosingBoxBracket,
+    Comma,
+    Semicolon,
+    Colon,
+    Point,
+    Arrow,
+    Inc,
+    Dec,
+    Add,
+    Sub,
+    Asterisk,
+    Div,
+    Mod,
+    Not,
+    BitwiseNot,
+    Ampersand,
+    Shl,
+    Shr,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+    Eq,
+    Ne,
+    Xor,
+    Or,
+    LAnd,
+    LOr,
+    Question,
+    Assign,
+    AssignAdd,
+    AssignSub,
+    AssignMul,
+    AssignDiv,
+    AssignMod,
+    AssignShl,
+    AssignShr,
+    AssignAnd,
+    AssignXor,
+    AssignOr,
+    Hash,
 }
