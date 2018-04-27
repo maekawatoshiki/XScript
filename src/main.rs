0ff79bb53291d3d6cae3a1d260f8c44b3b337e84
maekawatoshiki <konndennsa@gmail.com>
2018-04-27 22:00:02 +0900

    update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate clap;
+use clap::{App, Arg};
+
+const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("rcaml")
+        .version(VERSION_STR)
+        .author("uint256_t")
+        .about("XScript is a statically-typed script language with JIT.")
+        .arg(
+            Arg::with_name("version")
+                .short("v")
+                .long("version")
+                .help("Show version info"),
+        )
+        .arg(Arg::with_name("FILE").help("Input file").index(1))
+        .get_matches();
+
+    if app.is_present("version") {
+        println!("xscript {}", VERSION_STR);
+        return;
+    } else if let Some(filename) = app.value_of("FILE") {
+    } else {
+    }
 }
